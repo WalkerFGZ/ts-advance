8cf9fec05f45e09451664e7122e3f084376a2d93
Andres Gallego <andresfelipegallegozuleta@gmail.com>
2025-02-11 19:25:17 -0500

    inherence

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,12 +1,15 @@
 import { addProduct } from './products/product.service';
 
 addProduct({
-  id: 1,
-  title: 'Camisa',
-  createdAt: new Date().toDateString(),
-  stock: 100,
+  id: crypto.randomUUID(),
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  title: 'Product 1',
+  stock: 10,
   category: {
     id: 1,
-    name: 'Ropa',
+    name: 'Category 1',
+    createdAt: new Date(),
+    updatedAt: new Date(),
   },
 });
